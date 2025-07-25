37a32131fde37ad814ba79b55fa9b08f9714694c
KCLP1995 <chhaylymea43154001@gmail.com>
2025-07-25 08:07:21 +0700

    1

--- WebTools/main.py
+++ WebTools/main.py
@@ -95,10 +95,6 @@ def main():
 
     print(f"| 6. Component Version Enumerator                 | Identify versions of web components to find outdated or vulnerable ones." + " "*7 + "|")
 
-    print("|" + "_"*49 + "|" + "_"*80 + "|")
-
-    print(f"| 7. SSRF Detection Tool                          | Detect Server-Side Request Forgery vulnerabilities." + " "*28 + "|")
-
     # Bottom line
     print("|" + "_"*49 + "|" + "_"*80 + "|")
 
